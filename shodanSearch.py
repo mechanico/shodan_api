0fa5e350067280f1fe33b8ff8581bae708c5ca56
mezdanak <32109749+mechanico@users.noreply.github.com>
2020-01-09 12:48:26 +0100

    Switched arg syntax.

--- shodanSearch.py
+++ shodanSearch.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 	argparser = argparse.ArgumentParser()
 	argparser.add_argument('-s', '--search', type=str, help='search query', default='tornado',
 							action='store', required=True)
-	argparser.add_argument('-f', '--file', type=str, help='output filename', default='out.txt',
+	argparser.add_argument('-o', '--output', type=str, help='output filename', default='out.txt',
 							action='store', required=True)
 	args = argparser.parse_args()
 	try:
@@ -37,12 +37,12 @@ if __name__ == '__main__':
 		print('[+] Info: Total {0} results.'.format(str(results['total'])))
 		perform_query = input('[+] Input: Do you want to perform the query? This will cost you {0} shodan Query Credits [Y/N]: '.format(pages))
 		if perform_query.upper() == 'Y':
-			print('[+] Info: Fetching results from {0} pages. Results will be written to {1}. Shodan.io API calls are time throttled 1/sec.'.format(pages, args.file))
+			print('[+] Info: Fetching results from {0} pages. Results will be written to {1}. Shodan.io API calls are time throttled 1/sec.'.format(pages, args.output))
 			for x in range(0,pages):
 				print('[+] Info: Fetching page {0}'.format(x))
 				results_page = api.search(args.search, page=x)
 				try:
-					with open(args.file, 'a') as result_file:
+					with open(args.output, 'a') as result_file:
 						try:
 							for item in results_page['matches']:
 								result_file.write(item['ip_str'] + ':' + str(item['port']) + '\n') # Configure your prefered output format here!
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 							print('[-] Error: Please check your defined output format. {0} not found.'.format(e))
 							sys.exit()
 				except IOError:
-					print('[-] Error: Cannot open {0}'.format(args.file))
+					print('[-] Error: Cannot open {0}'.format(args.output))
 					sys.exit()
 			print('[+] Info: Finished.\n')
 		else:
