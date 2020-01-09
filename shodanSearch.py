3b9e30f8ac156cea2ab5f7f5f32e3743abfd6d3e
mezdanak <32109749+mechanico@users.noreply.github.com>
2020-01-09 12:45:00 +0100

    Update for Python3 and minor cleanups

--- shodanSearch.py
+++ shodanSearch.py
@@ -1,58 +1,60 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 '''
 author: @mezdanak, modzero AG, https://www.modzero.ch, @mod0
+usage: $ python shodanSearch.py -s 'shodanSearchTerm' -o results.txt
+
+This tool can be used to retrieve IP-address:port combinations from a shodan.io search (argument -s). 
+Results will be stored in the file specified (argument -o).
 '''
 import sys
-import urllib2
-import socket
-import httplib
 import argparse
 
 try:
 	import shodan
 except ImportError:
-	print "[-] Error: Cannot import 'shodan', run $ sudo pip install shodan"
+	print('[-] Error: Cannot import \'shodan\', run $ sudo pip3 install shodan')
 	sys.exit()
 
 if __name__ == '__main__':
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument('-s', '--search', type=str, help='search query', default='foo',
-							action='store')
+	argparser.add_argument('-s', '--search', type=str, help='search query', default='tornado',
+							action='store', required=True)
 	argparser.add_argument('-f', '--file', type=str, help='output filename', default='out.txt',
-							action='store')
+							action='store', required=True)
 	args = argparser.parse_args()
 	try:
-		shodan_api_key = raw_input('[+] Input: Enter your shodan.io API key: ')
+		shodan_api_key = input('[+] Input: Enter your shodan.io API key: ')
 		if len(shodan_api_key) != 32:
-			print '[-] Error: Invalid shodan.io API key.'
+			print('[-] Error: Invalid shodan.io API key.')
 			sys.exit()
 		else:
 			api = shodan.Shodan(shodan_api_key)
 		results = api.search(args.search)
+		print(results)
 		results_total = results['total']
 		pages = int(round(results_total/100)) + 1
-		print '[+] Info: Total %s results.' %str(results['total'])
-		perform_query = raw_input('[+] Input: Do you want to perform the query? This will cost you %i shodan Query Credits [Y/N]: ' %pages)
+		print('[+] Info: Total {0} results.'.format(str(results['total'])))
+		perform_query = input('[+] Input: Do you want to perform the query? This will cost you {0} shodan Query Credits [Y/N]: '.format(pages))
 		if perform_query.upper() == 'Y':
-			print "[+] Info: Fetching results from %s pages. Results will be written to %s. Shodan.io API calls are time throttled 1/sec." %(pages, args.file)
+			print('[+] Info: Fetching results from {0} pages. Results will be written to {1}. Shodan.io API calls are time throttled 1/sec.'.format(pages, args.file))
 			for x in range(0,pages):
-				print '[+] Info: Fetching page %s' %x
+				print('[+] Info: Fetching page {0}'.format(x))
 				results_page = api.search(args.search, page=x)
 				try:
 					with open(args.file, 'a') as result_file:
 						try:
 							for item in results_page['matches']:
 								result_file.write(item['ip_str'] + ':' + str(item['port']) + '\n') # Configure your prefered output format here!
-						except KeyError, e:
-							print '[-] Error: Please check your defined output format. %s not found.' %e
+						except KeyError as e:
+							print('[-] Error: Please check your defined output format. {0} not found.'.format(e))
 							sys.exit()
 				except IOError:
-					print '[-] Error: Cannot open %s' %(args.file)
+					print('[-] Error: Cannot open {0}'.format(args.file))
 					sys.exit()
-			print '[+] Info: Finished.\n'
+			print('[+] Info: Finished.\n')
 		else:
-			print '[+] Info: Quitting program.'
+			print('[+] Info: Quitting program.')
 			sys.exit()
-	except shodan.APIError, e:
-		print '[-] Error: %s' %e
+	except shodan.APIError as e:
+		print('[-] Error: {0}'.format(e))
